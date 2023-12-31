8514d43d1df1aad8d153d17987d91dce1a2c40be
Junior Mabunda <mjmabunda0@gmail.com>
2023-12-31 20:36:12 +0200

    Fix print_octal function logic, include stdlib
    Changes:
    - Included stdlib header file for malloc
    - Corrected the for loop to print octal digits
    - Handle case when no is 0

--- print_octal.c
+++ print_octal.c
@@ -1,28 +1,45 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
  * print_octal - converts to octal
  * @val: variable parameter
  * Return: counter
 */
- int print_octal(va_list val)
- {
-    int MJ, *array, counter = 0; 
-    unsigned int no = va_arg(val, unsigned int);
-    unsigned int tmpry = no;
-    while (no / 8 != 0)
-    {
-        no = no / 8;
-        counter++;
-    }
-    counter++;
-    array = malloc(sizeof(int) * counter);
-       for(MJ = 0; MJ < counter; MJ++)
-    {
-        array[MJ] = tmpry % 8;
-        tmpry = tmpry / 8;
-    }
-    for(MJ = counter - 1; MJ >= 0; MJ--)
-    _putchar(array[MJ] = '0');
-    free(array);
-    return(counter);
- }
+int print_octal(va_list val)
+{
+	int MJ, *array, counter;
+	unsigned int no;
+	unsigned int tmpry;
+
+	counter = 0;
+	no = va_arg(val, unsigned int);
+	tmpry = no;
+
+	if (no == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	while (no != 0)
+	{
+		no = no / 8;
+		counter++;
+	}
+	
+	array = malloc(sizeof(int) * counter);
+	no = tmpry;
+	
+	for (MJ = 0; MJ < counter; MJ++)
+	{
+		array[MJ] = tmpry % 8;
+		tmpry = tmpry / 8;
+	}
+
+	for (MJ = counter - 1; MJ >= 0; MJ--)
+		_putchar(array[MJ] + '0');
+
+	free(array);
+	return (counter);
+}
