46ea08312d2d90eba050cee0324333993071bbf8
Junior Mabunda <mjmabunda0@gmail.com>
2023-12-31 13:22:03 +0200

    Update README and _putchar function to follow betty guidelines

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,13 @@
-#include "main.h"
-/*
-*_putchar - writes the character c to stdout
-&c : the character to print
+#include <unistd.h>
 
-Return: on success
-on error: -1 is returned, and error is set appropriately
-*/
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
-return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
